d58292b0cc28683130f19ec5e244f124bba497db
gibrahimg <gumma.ibrahim@gmail.com>
2024-05-19 12:16:28 -0700

    Initial commit

---
+++ functions.py
@@ -0,0 +1,17 @@
+FILEPATH = "todos.txt"
+
+
+def get_todos(filepath=FILEPATH):
+    """
+    read a text file and load
+    a list of to-do items
+    """
+    with open(filepath, 'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+
+def write_todos(todos_arg, filepath=FILEPATH):
+    """write to-do items to the text file"""
+    with open(filepath, 'w') as file:
+        file.writelines(todos_arg)
